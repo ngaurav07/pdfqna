791021c8747eb5cdd204e96693901f81d3561e2f
Gaurav Neupane <ngaurav456@gmail.com>
2024-06-29 10:21:24 -0500

    feat(docker): create the docker file and compose for app

--- helpers/model_loader.py
+++ helpers/model_loader.py
@@ -1,19 +1,31 @@
 from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain.chains.question_answering import load_qa_chain
+import os
 
 #Initialize the open source llama models
 def _initialize_model(model: str = 'llama3'):
     try:
-        model = ChatOllama(model=model)
+        # base_url = os.environ['OLLAMA_HOST_URL']
+        base_url = "http://ollama:11434"
+        print("base url is", base_url)
+        model = ChatOllama(
+            base_url=base_url,
+            model=model)
         return model
     except:
         raise Exception("Unable to initialize chat model")
 
+
 #initialize the embedding model to store in the vector database
 def initialize_embedding(model: str = 'mxbai-embed-large'):
     try:
-        embedding_model = OllamaEmbeddings(model=model)
+        # base_url = os.environ['OLLAMA_HOST_URL']
+        base_url = "http://ollama:11434"
+        print("embedding", base_url)
+        embedding_model = OllamaEmbeddings(
+            base_url=base_url,
+            model=model)
         return embedding_model
     except:
         raise Exception("Unable to initialize the embedding model")
--- helpers/vector_store.py
+++ helpers/vector_store.py
@@ -4,6 +4,7 @@ from helpers.model_loader import initialize_embedding
 #intialize the vector database which will store the contents with the embeddings
 def initialize_store(chunked_documents):
     try:
+        print("I am being called")
         vectordb = Chroma.from_documents(
             documents=chunked_documents,
             embedding= initialize_embedding()
